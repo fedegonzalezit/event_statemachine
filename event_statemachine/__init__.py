eac25bf20390ee50bb570ab1b5502abae40483aa
Federico Gonzalez <fedelean.gon@gmail.com>
2023-09-26 22:21:59 -0300

    cambio de lugar en los imports

--- event_statemachine/__init__.py
+++ event_statemachine/__init__.py
@@ -1,12 +1,11 @@
 """It is a simple state machine library, based on events.
 It is easy to use, extend and scale."""
-
-__author__ = """Federico Gonzalez Itzik"""
-__email__ = "fedelean.gon@gmail.com"
-__version__ = "0.0.1"
-
 from event_statemachine.sm import event_condition  # noqa
 from event_statemachine.sm import on_state_entry  # noqa
 from event_statemachine.sm import on_state_exit  # noqa
 from event_statemachine.sm import transition  # noqa
 from event_statemachine.sm import StateMachine  # noqa
+
+__author__ = """Federico Gonzalez Itzik"""
+__email__ = "fedelean.gon@gmail.com"
+__version__ = "0.0.1"
