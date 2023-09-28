9aa2a8e22587df57ddef025853da59d88344cf6a
Federico Gonzalez <fedelean.gon@gmail.com>
2023-09-28 09:17:47 -0300

    new description

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    description="A simple event driven statemachine",
+    description="A simple event driven state machine",
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
